9be69c620fd282ddc7a341736756aa87d2a3d6bb
alekspo <aleksey.popryaduhin@thedaedals.com>
2017-04-17 16:24:33 +0300

    Drag&Drop.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,20 @@
 var COOKIE_NAME = 'name';
+var COOKIE_AUTH = 'auth';
 var IN_MESSAGE = 1;
 var OUT_MESSAGE = 0;
 var SERVICE_MESSAGE = 2;
+var SERVER_URL = 'https://test.habarshi.com:11999';
+var UPLOAD_URL;
+var USERNAME = 'director';
+var PASSWORD = 'pass';
+var MAX_FILE_SIZE = 50000000; // максимальный размер файла - 50 мб.
 
 $(document).ready(function () {
-    var name = getCookie(COOKIE_NAME);
+    if (getCookie(COOKIE_AUTH) === 'false' || getCookie(COOKIE_AUTH) === undefined) {
+        auth();
+    }
+    refineUpload();
+    var name = getUserName();
     if (name === undefined) {
         disableChat(true);
     } else {
@@ -20,6 +30,109 @@ $(document).ready(function () {
     });
 });
 
+function refineUpload() {
+    if (typeof(window.FileReader) === 'undefined') {
+        alert('Drag&Drop не поддерживается браузером!');
+    }
+    var dropZone = $('#attachButton');
+    var attachIcon = $('#attachIcon');
+    dropZone.click(function () {
+        $('#selectFile').click();
+    });
+    dropZone[0].ondragover = function () {
+        dropZone.addClass('hover');
+        attachIcon.removeClass('fa-paperclip');
+        attachIcon.addClass('fa-plus');
+        return false;
+    };
+
+    dropZone[0].ondragleave = function () {
+        dropZone.removeClass('hover');
+        attachIcon.addClass('fa-paperclip');
+        attachIcon.removeClass('fa-plus');
+        return false;
+    };
+    dropZone[0].ondrop = function (event) {
+        event.preventDefault();
+        dropZone.removeClass('hover');
+        dropZone.addClass('drop');
+        var file = event.dataTransfer.files[0];
+
+        if (file.size > MAX_FILE_SIZE) {
+            alert('Файл слишком большой!');
+            dropZone.addClass('error');
+            return false;
+        }
+        console.log(file);
+        var formData = new FormData();
+        formData.append('file', file);
+        var options = {
+            url: UPLOAD_URL,
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            xhr: function () {
+                var xhr = $.ajaxSettings.xhr();
+                xhr.upload.onprogress = uploadProgress;
+                return xhr;
+            }
+        };
+        $.ajax(options)
+            .done(function (data) {
+                console.log(data);
+                var response = JSON.parse(data);
+                var fileLink = '<a href="' + response['full_url'] + '" target="_blank">' + file.name + '</a>';
+                var message = getUserName() + ' загрузил файл ' + fileLink;
+                appendMessage(message, SERVICE_MESSAGE);
+                dropZone.removeClass('drop');
+                attachIcon.addClass('fa-paperclip');
+                attachIcon.removeClass('fa-plus');
+            })
+            .fail(function (xhr, message) {
+                console.log(message);
+                alert('Ошибка во время загрузки файлйа!');
+                dropZone.addClass('error');
+                attachIcon.addClass('fa-paperclip');
+                attachIcon.removeClass('fa-plus');
+            });
+    };
+
+    function stateChange(event) {
+        if (event.target.readyState === 4) {
+            if (event.target.status === 200) {
+                alert('Загрузка успешно завершена!');
+            } else {
+                console.log(event.target);
+                alert('Произошла ошибка!');
+                dropZone.addClass('error');
+            }
+        }
+    }
+
+}
+
+function uploadProgress(event) {
+    var percent = parseInt(event.loaded / event.total * 100);
+    console.log('Загрузка: ' + percent + '%');
+}
+
+function auth() {
+    $.post(SERVER_URL + '/auth/1?username=' + USERNAME + '&password=' + PASSWORD, function (data) {
+        var uploads = JSON.parse(data)['uploads'];
+        if (uploads === undefined) {
+            console.log(data);
+            alert('Не удалось авторизоваться');
+            return;
+        }
+        UPLOAD_URL = 'http://' + uploads['address'] + ':' + uploads['port'] + '/upload';
+        console.log(data);
+    }).fail(function (xhr, message) {
+        alert('Не удалось авторизоваться');
+        console.log(message);
+    });
+}
+
 function submit(e, f) {
     if (e.which === 13) {
         f.call();
@@ -28,8 +141,7 @@ function submit(e, f) {
 }
 
 function sendMessage() {
-    var message = createMessageHTML($('#messageInput').val(), OUT_MESSAGE);
-    appendMessage(message);
+    appendMessage($('#messageInput').val(), OUT_MESSAGE);
     $('#messageInput').val('');
 }
 
@@ -37,12 +149,12 @@ function login() {
     setCookie(COOKIE_NAME, $('#loginName').val());
     $('#loginName').val('');
     disableChat(false);
-    appendMessage(createMessageHTML('Вы вошли в чат как ' + getCookie(COOKIE_NAME), SERVICE_MESSAGE));
+    appendMessage('Вы вошли в чат как ' + getUserName(), SERVICE_MESSAGE);
 }
 
-function appendMessage(message) {
+function appendMessage(message, type) {
     var $messages = $('#messages');
-    $messages.append(message);
+    $messages.append(createMessageHTML(message, type));
     $messages.animate({scrollTop: $messages.scrollTop() + $messages.height()}, "slow");
 }
 
@@ -57,10 +169,14 @@ function createMessageHTML(message, type) {
         return '<div class="srv-msg message">' + message + '</div>';
     } else {
         return '<div style="overflow: hidden;"><div class="' + (type === IN_MESSAGE ? 'in' : 'out') + '-msg message">'
-            + (type === OUT_MESSAGE ? '<p><b>' + getCookie(COOKIE_NAME) + '</b> ' + new Date().toLocaleTimeString() + '</p>' : '')
+            + (type === OUT_MESSAGE ? '<p><b>' + getUserName() + '</b> ' + new Date().toLocaleTimeString() + '</p>' : '')
             + '<div>' + message + '</div>'
             + '</div></div>';
     }
+}
+
+function getUserName() {
+    return getCookie(COOKIE_NAME);
 }
 
 // возвращает cookie с именем name, если есть, если нет, то undefined
